6c623c5946937c2c43593127dc64e372cfb05e06
Justen Wolf <53807308+disarrangedwolf@users.noreply.github.com>
2026-07-01 15:03:01 -0700

    gf

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,5 @@
 export const siteConfig = {
-  brand: {
-    name: 'DisarrangedWolf',
-    logoText: './DisarrangedWOlf',
-  },
    contact: {
     email: 'wolf@disarrangedwolf.com',
   },
-  socialLinks: [
-    { label: 'GitHub', href: 'https://github.com/disarrangedwolf' },
-    { label: 'LinkedIn', href: 'https://www.linkedin.com/' },
-  ],
   }
