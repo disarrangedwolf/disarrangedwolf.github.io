3f5bfb40f026864b10371e5eaeec61f855c48c0d
Justen Wolf <53807308+disarrangedwolf@users.noreply.github.com>
2026-07-01 15:04:31 -0700

    Revert "gf"
    This reverts commit 6c623c5946937c2c43593127dc64e372cfb05e06.

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,13 @@
 export const siteConfig = {
+  brand: {
+    name: 'DisarrangedWolf',
+    logoText: './DisarrangedWOlf',
+  },
    contact: {
     email: 'wolf@disarrangedwolf.com',
   },
+  socialLinks: [
+    { label: 'GitHub', href: 'https://github.com/disarrangedwolf' },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/' },
+  ],
   }
